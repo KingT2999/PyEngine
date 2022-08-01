84d7b5988645d9095c9809ef3dff466b0b40d149
KingT2999 <tbektashyan@inbox.ru>
2022-08-01 14:59:07 +0300

    music

--- scripts.py
+++ scripts.py
@@ -9,7 +9,7 @@ from engine.components import *
 @Start
 def music_start():
 	pygame.mixer.music.load('media/audio/music.mp3')
-	pygame.mixer.music.play()
+	pygame.mixer.music.play(loops=1)
 
 @Start
 def player_start() -> None:
